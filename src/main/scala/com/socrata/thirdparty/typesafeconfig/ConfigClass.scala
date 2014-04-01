1b434d7db6d6202100b969014b6ee423bac90faa
Robert J. Macomber <robert.macomber@socrata.com>
2014-04-01 10:50:02 -0700

    Add getBytes, getBoolean, and getObjectOf

--- src/main/scala/com/socrata/thirdparty/typesafeconfig/ConfigClass.scala
+++ src/main/scala/com/socrata/thirdparty/typesafeconfig/ConfigClass.scala
@@ -1,20 +1,26 @@
 package com.socrata.thirdparty.typesafeconfig
 
-import com.typesafe.config.{Config, ConfigException}
+import com.typesafe.config.{Config, ConfigException, ConfigUtil}
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.duration._
 import scala.collection.JavaConverters._
 
 /** A helper for making typed configurations. */
 abstract class ConfigClass(config: Config, root: String) {
-  protected def path(key: String) = root + "." + key
+  protected def path(key: String*) = root + "." + ConfigUtil.joinPath(key : _*)
 
   // we'll add more of these as we need them
   def getInt(key: String): Int = config.getInt(path(key))
   def getString(key: String): String = config.getString(path(key))
   def getStringList(key: String): Seq[String] = config.getStringList(path(key)).asScala
   def getDuration(key: String): FiniteDuration = config.getMilliseconds(path(key)).longValue.millis
+  def getBytes(key: String): Long = config.getBytes(path(key)).longValue
+  def getBoolean(key: String): Boolean = config.getBoolean(path(key))
   def getConfig[T](key: String, decoder: (Config, String) => T): T = decoder(config, path(key))
+  def getObjectOf[T](key: String, decoder: (Config, String) => T): Map[String, T] =
+    config.getObject(path(key)).keySet.asScala.toSeq.map { k =>
+      k -> decoder(config, path(key, k))
+    }.toMap
   def getRawConfig(key: String): Config = config.getConfig(path(key))
   def optionally[T](e: => T): Option[T] = try {
     Some(e)
