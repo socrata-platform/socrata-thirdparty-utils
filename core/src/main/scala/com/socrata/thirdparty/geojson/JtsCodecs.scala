14ec117f378fa8dc774994e3e05cd167c29ddfde
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2014-11-17 18:52:10 -0800

    Support JsonCodec.encode for JtsCodecs

--- core/src/main/scala/com/socrata/thirdparty/geojson/JtsCodecs.scala
+++ core/src/main/scala/com/socrata/thirdparty/geojson/JtsCodecs.scala
@@ -19,7 +19,13 @@ object JtsCodecs {
   private val factory = new GeometryFactory
 
   implicit object CoordinateCodec extends JsonCodec[Coordinate] {
-    def encode(pt: Coordinate): JValue = ???
+    def encode(pt: Coordinate): JValue =
+      if (pt.z.isNaN) {
+        JArray(Seq(JNumber(pt.x), JNumber(pt.y)))
+      } else {
+        JArray(Seq(JNumber(pt.x), JNumber(pt.y), JNumber(pt.z)))
+      }
+
     def decode(json: JValue): Option[Coordinate] = json match {
       case JArray(Seq(JNumber(x), JNumber(y))) =>
         Some(new Coordinate(x.toDouble, y.toDouble))
@@ -31,19 +37,26 @@ object JtsCodecs {
   }
 
   implicit object PointCodec extends JsonCodec[Point] {
-    def encode(pt: Point): JValue = ???
+    def encode(pt: Point): JValue = JsonCodec[Coordinate].encode(pt.getCoordinate)
     def decode(json: JValue): Option[Point] =
       CoordinateCodec.decode(json).map { coord => factory.createPoint(coord) }
   }
 
   implicit object LineStringCodec extends JsonCodec[LineString] {
-    def encode(line: LineString): JValue = ???
+    def encode(line: LineString): JValue = JsonCodec[Array[Coordinate]].encode(line.getCoordinates)
     def decode(json: JValue): Option[LineString] =
       JsonCodec[Array[Coordinate]].decode(json).map { coords => factory.createLineString(coords) }
   }
 
   implicit object PolygonCodec extends JsonCodec[Polygon] {
-    def encode(polygon: Polygon): JValue = ???
+    def encode(polygon: Polygon): JValue = {
+      val exteriorRing = polygon.getExteriorRing.getCoordinates
+      val interiorRings = (0 to polygon.getNumInteriorRing - 1).map { idx =>
+        polygon.getInteriorRingN(idx).getCoordinates
+      }
+      JsonCodec[List[Array[Coordinate]]].encode(List(exteriorRing) ++ interiorRings)
+    }
+
     def decode(json: JValue): Option[Polygon] = {
       for {
         // Splits the first ring of a polygon from subsequent rings, which are holes.
@@ -58,13 +71,21 @@ object JtsCodecs {
   }
 
   implicit object MultiPolygonCodec extends JsonCodec[MultiPolygon] {
-    def encode(mp: MultiPolygon): JValue = ???
+    def encode(mp: MultiPolygon): JValue =
+      JsonCodec[Array[Polygon]].encode((0 to mp.getNumGeometries - 1).map { idx =>
+        mp.getGeometryN(idx) match { case p: Polygon => p }
+      }.toArray)
+
     def decode(json: JValue): Option[MultiPolygon] =
       JsonCodec[Array[Polygon]].decode(json).map { polygons => factory.createMultiPolygon(polygons) }
   }
 
   implicit object MultiLineStringCodec extends JsonCodec[MultiLineString] {
-    def encode(mls: MultiLineString): JValue = ???
+    def encode(mls: MultiLineString): JValue =
+      JsonCodec[Array[LineString]].encode((0 to mls.getNumGeometries - 1).map { idx =>
+        mls.getGeometryN(idx) match { case ls: LineString => ls }
+      }.toArray)
+
     def decode(json: JValue): Option[MultiLineString] =
       JsonCodec[Array[LineString]].decode(json).map { lines => factory.createMultiLineString(lines) }
   }
