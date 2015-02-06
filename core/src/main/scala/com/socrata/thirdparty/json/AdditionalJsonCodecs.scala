19028dd03276ce320724c7ea90ca5921b76686b0
Chi Tang <chi.tang@socrata.com>
2015-02-06 10:41:29 -0800

    Use wrapper json codec.

--- core/src/main/scala/com/socrata/thirdparty/json/AdditionalJsonCodecs.scala
+++ core/src/main/scala/com/socrata/thirdparty/json/AdditionalJsonCodecs.scala
@@ -1,29 +1,16 @@
 package com.socrata.thirdparty.json
 
-import com.rojoma.json.v3.ast.{JString, JValue}
-import com.rojoma.json.v3.codec.DecodeError.{InvalidType, InvalidValue}
-import com.rojoma.json.v3.codec.JsonDecode._
-import com.rojoma.json.v3.codec.{JsonDecode, JsonEncode}
+import com.rojoma.json.v3.util.WrapperJsonCodec
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
+
 object AdditionalJsonCodecs {
 
-  implicit object DateTimeCodec extends JsonEncode[DateTime] with JsonDecode[DateTime] {
-    val formatter = ISODateTimeFormat.dateTime
+  implicit val dateTimeCodec = locally {
+    val formatter = ISODateTimeFormat.dateTime.withZoneUTC
     val parser = ISODateTimeFormat.dateTimeParser
 
-    def encode(x: DateTime): JValue = JString(formatter.print(x))
-
-    def decode(x: JValue): DecodeResult[DateTime] = x match {
-      case JString(s) =>
-        try {
-          Right(parser.parseDateTime(s))
-        } catch {
-          case _: IllegalArgumentException =>
-            Left(InvalidValue(x))
-        }
-      case u => Left(InvalidType(JString, u.jsonType))
-    }
+    WrapperJsonCodec[DateTime](parser.parseDateTime, formatter.print)
   }
 }
