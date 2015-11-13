52a32ea18fed087eefd9b8250242b157b8e61e43
Alexa Rust <alexa.rust@socrata.com>
2015-11-13 15:03:59 -0800

    Remove broken link from scala doc

--- core/src/main/scala/com/socrata/thirdparty/metrics/SocrataHttpSupport.scala
+++ core/src/main/scala/com/socrata/thirdparty/metrics/SocrataHttpSupport.scala
@@ -11,7 +11,7 @@ object SocrataHttpSupport {
   /**
    * Function to wrap an existing Jetty handler with an InstrumentedHandler.
    * See https://dropwizard.github.io/metrics/3.1.0/manual/jetty/.
-   * Can be used with socrata-http; see [[AbstractSocrataServerJetty.Options]].
+   * Can be used with socrata-http.
    * Set extraHandlers = List(SocrataHttpSupport.getHandler(options)).
    *
    * @param options a MetricsOptions for setting the metrics prefix
